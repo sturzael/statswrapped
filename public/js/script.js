06f7e0921ee4b4a2bbc97babe533902cff5d3535
sturzael <esturz23@gmail.com>
2018-10-02 21:40:15 +1300

    added basic vue.js components

--- public/js/script.js
+++ public/js/script.js
@@ -7,43 +7,52 @@
   function getHashParams() {
     var hashParams = {};
     var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    while ( e = r.exec(q)) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
+      q = window.location.hash.substring(1);
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
   }
 
-  var userProfileSource = document.getElementById('user-profile-template').innerHTML,
-      userProfileTemplate = Handlebars.compile(userProfileSource),
-      userProfilePlaceholder = document.getElementById('user-profile');
+  // const userProfileSource = document.getElementById('user-profile-template').innerHTML,
+  //     userProfileTemplate = Handlebars.compile(userProfileSource);
 
   var params = getHashParams();
 
-  var access_token = params.access_token,
-      refresh_token = params.refresh_token,
-      error = params.error;
+  let access_token = params.access_token,
+    refresh_token = params.refresh_token,
+    error = params.error;
 
   if (error) {
     alert('There was an error during the authentication');
   } else {
     if (access_token) {
       $.ajax({
-          url: 'https://api.spotify.com/v1/me',
-          headers: {
-            'Authorization': 'Bearer ' + access_token
-          },
-          success: function(response) {
-            userProfilePlaceholder.innerHTML = userProfileTemplate(response);
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+          'Authorization': 'Bearer ' + access_token
+        },
+        success: function(response) {
+          $('#login').hide();
+          $('.container').load('loggedin.html',function(){
+            new Vue({
+              el: '#loggedin',
+              data: {
+                display_name: response.display_name,
+                id: response.id,
+                profile_picture: response.images[0].url,
+              }
+            })
+          });
 
-            $('#login').hide();
-            $('#loggedin').show();
-          }
+
+
+        }
       });
     } else {
-        // render initial screen
-        $('#login').show();
-        $('#loggedin').hide();
+      // render initial screen
+      $('.container').load('login.html');
+      $('#loggedin').hide();
     }
   }
 })();
